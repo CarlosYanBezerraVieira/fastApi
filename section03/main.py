4431cd7fc4fb01c1510f3186b9d4cd9ff54ad65a
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-03 12:25:58 -0300

    feat: implemented method put

--- section03/main.py
+++ section03/main.py
@@ -39,6 +39,15 @@ async def post_curso(curso: Curso):
         del curso.id
         return  curso
 
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id:int, curso:Curso):
+     if curso_id in cursos:
+          cursos[curso_id] = curso
+        #fazendo isso só pra resposta não ficar null
+          del curso.id
+          return curso
+     else:
+          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe curso com o {curso_id} ')   
 
 if __name__ == '__main__':
     import uvicorn
