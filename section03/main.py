ea322326876e9361409e818fe51bfdeaa92ba3bf
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-03 11:51:20 -0300

    feat: implemented method post and set status code right

--- section03/main.py
+++ section03/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+from models import Curso
 
 app = FastAPI()
 
 cursos = {
     1:{
-        'itulo': 'programaçãom pra leigos',
+        'titulo': 'programaçãom pra leigos',
         'aulas':112,
         'horas':58
     },
@@ -28,8 +29,17 @@ async def get_curso(curso_id: int):
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+    
+@app.post('/cursos', status_code= status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+        #gambiara para quando não tem id
+        next_id = len(cursos)+1
+        cursos[next_id] = curso
+        #fazendo isso só pra resposta não ficar null
+        del curso.id
+        return  curso
 
- 
+
 if __name__ == '__main__':
     import uvicorn
     
