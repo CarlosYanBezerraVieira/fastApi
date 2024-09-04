9074f0c98e4c516fc40d7a80c7f4982e89cf9072
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-04 10:20:22 -0300

    feat: implemented inject dependencies

--- section03/main.py
+++ section03/main.py
@@ -6,7 +6,19 @@ from fastapi import Response
 from fastapi import Path
 from fastapi import Query
 from fastapi import Header
-from typing import Optional
+from typing import Optional, Any
+from time import sleep
+from  fastapi import Depends
+
+def fake_db():
+    try:
+          print('Abrindo conexão com banco de dados...')
+          sleep(1)
+    finally:
+          print('Fechando conexão com banco de dados...')
+          sleep(1)
+         
+        
 
 app = FastAPI()
 
@@ -24,13 +36,14 @@ cursos = {
 }
 
 @app.get('/cursos')
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db) ):
     return cursos
 
 @app.get('/cursos/{curso_id}')
 async def get_curso(curso_id: int = 
                     #adicionar uma validação  a mais
-                    Path(default=None, title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)
+                    Path(default=None, title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3),
+                    db: Any = Depends(fake_db)
                     ):
     try:
         curso = cursos[curso_id]
@@ -39,7 +52,8 @@ async def get_curso(curso_id: int =
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
     
 @app.post('/cursos', status_code= status.HTTP_201_CREATED)
-async def post_curso(curso: Curso):
+async def post_curso(curso: Curso,
+                     db: Any = Depends(fake_db)):
         #gambiara para quando não tem id
         next_id = len(cursos)+1
         cursos[next_id] = curso
@@ -48,7 +62,8 @@ async def post_curso(curso: Curso):
         return  curso
 
 @app.put('/cursos/{curso_id}')
-async def put_curso(curso_id:int, curso:Curso):
+async def put_curso(curso_id:int, curso:Curso,
+                    db: Any = Depends(fake_db)):
      if curso_id in cursos:
           cursos[curso_id] = curso
         #fazendo isso só pra resposta não ficar null
@@ -57,7 +72,8 @@ async def put_curso(curso_id:int, curso:Curso):
      else:
           raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe curso com o {curso_id} ')
 @app.delete('/cursos/{curso_id}')
-async def delete_curso(curso_id:int):
+async def delete_curso(curso_id:int,
+                       db: Any = Depends(fake_db)):
      if curso_id in cursos:
           del cursos[curso_id]
           #bug no fast api não usar por enquanto
