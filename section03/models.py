ba396e2ef857694bf4b60c9f80cf80633585e44a
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-04 12:17:03 -0300

    feat: create new list of cursos

--- section03/models.py
+++ section03/models.py
@@ -2,8 +2,15 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+
 class Curso(BaseModel):
-    id: Optional[int]= None
+    id: Optional[int] = None
     titulo: str
-    aulas:int
-    horas:int
+    aulas: int
+    horas: int
+
+
+cursos = [
+    Curso(id=1, titulo="programaçãom pra leigos", aulas=112, horas=58),
+    Curso(id=2, titulo="programaçãom pra leigos2", aulas=59, horas=30),
+]
