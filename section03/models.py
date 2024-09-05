854d75888306beb3cc7805d699932677de1c7950
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-05 11:36:37 -0300

    feat: adding validations

--- section03/models.py
+++ section03/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Curso(BaseModel):
@@ -9,8 +9,19 @@ class Curso(BaseModel):
     aulas: int
     horas: int
 
+    # deve seguir esse padrão de parametros
+    @validator("titulo")
+    def validar_titulo(cls, value: str):
+        palavras = value.split(" ")
+        # validações
+        if len(palavras) < 3:
+            raise ValueError("O titulo deve ter pelo menos 3 palavras")
+        if value.islower():
+            raise ValueError("O titulo deve ser capitalizado")
+        return value
+
 
 cursos = [
-    Curso(id=1, titulo="programaçãom pra leigos", aulas=112, horas=58),
-    Curso(id=2, titulo="programaçãom pra leigos2", aulas=59, horas=30),
+    Curso(id=1, titulo="Programação Para Leigos", aulas=112, horas=58),
+    Curso(id=2, titulo="Programação Para Leigos 2", aulas=59, horas=30),
 ]
