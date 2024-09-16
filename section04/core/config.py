7a64dbbe81d758689bf8698da046bdc011fdbef4
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-16 10:40:31 -0300

    fix: connection with my database

--- section04/core/config.py
+++ section04/core/config.py
@@ -1,4 +1,6 @@
-from pydantic import BaseSettings
+from typing import ClassVar
+
+from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -6,8 +8,8 @@ class Settings(BaseSettings):
     # Configurações gerais usadas na aplicação
 
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = "postgresql+asyncpg://carlos:university@localhost:5432/faculdade"
-    DBBaseModel = declarative_base()
+    DB_URL: str = "postgresql+asyncpg://carlos:123@localhost:5432/faculdade"
+    DBBaseModel: ClassVar = declarative_base()
 
     class Config:
         case_sensitive = True
