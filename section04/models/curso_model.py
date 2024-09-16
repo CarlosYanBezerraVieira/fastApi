13335f97cda5ac7ec8ef1bd7be6f3b2732108171
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-09-16 10:40:47 -0300

    fix: model curso model

--- section04/models/curso_model.py
+++ section04/models/curso_model.py
@@ -3,14 +3,8 @@ from sqlalchemy import Column, Integer, String
 
 
 class CursoModel(settings.DBBaseModel):
-    __tablename = "cursos"
-    id: int = (
-        Column(
-            Integer,
-            primary_key=True,
-            autoincrement=True,
-        ),
-    )
+    __tablename__ = "cursos"
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
     titulo: str = Column(String(100))
     aulas: int = Column(Integer)
     horas: int = Column(Integer)
